35d73093b5c0f6817d57cde6bbcb205600a13737
willow hughes <99459216+willowhughes@users.noreply.github.com>
2023-05-13 14:54:44 -0700

    recycler view is now created in home fragment and isn't destroyed from navigating around

--- app/src/main/java/com/example/yarnify/MainActivity.java
+++ app/src/main/java/com/example/yarnify/MainActivity.java
@@ -18,10 +18,6 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private RecyclerView mRecyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
-    ArrayList<patternObject> exampleList = new ArrayList<>(); //array of pattern objects
 
     private ActivityMainBinding binding;
 
@@ -44,22 +40,6 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
 
-        //hardcoded example pattern objects
-        exampleList.add(new patternObject(R.drawable.ravelry_sample_photo, "gloves", "Susan"));
-        exampleList.add(new patternObject(R.drawable.armillafirm_small2, "thin sweater", "Jack"));
-        exampleList.add(new patternObject(R.drawable.beanies_medium2, "beanies", "Bob"));
 
-        setUpRecyclerView(); //method initializes and sets the recyclerview, adapter, and layout manager
     }
-
-    public void setUpRecyclerView() {
-        mRecyclerView = findViewById(R.id.recyclerView); //initializes a variable mRecyclerView and assigns it to the view with the ID recyclerView from activity_main.xml layout file
-        mRecyclerView.setHasFixedSize(true); //the size of the RecyclerView will remain constant and won't change as the contents of the adapter change
-        mLayoutManager = new LinearLayoutManager(this); //LinearLayoutManager lays out items in a RecyclerView in a vertical fashion
-        mAdapter = new cardAdapter(exampleList); //A cardAdapter is a custom adapter class that provides the data to the RecyclerView to display.
-        //The exampleList is passed to the constructor and contains the list of pattern objects that will be displayed in the RecyclerView.
-        mRecyclerView.setLayoutManager(mLayoutManager); //ensures that the RecyclerView knows how to lay out its items
-        mRecyclerView.setAdapter(mAdapter); //sets adapter
-    }
-
 }
--- app/src/main/java/com/example/yarnify/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/yarnify/ui/home/HomeFragment.java
@@ -9,10 +9,22 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.yarnify.R;
+import com.example.yarnify.cardAdapter;
 import com.example.yarnify.databinding.FragmentHomeBinding;
+import com.example.yarnify.patternObject;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    ArrayList<patternObject> exampleList = new ArrayList<>(); //array of pattern objects
 
     private FragmentHomeBinding binding;
 
@@ -24,6 +36,12 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        //hardcoded example pattern objects
+        exampleList.add(new patternObject(R.drawable.ravelry_sample_photo, "gloves", "Susan"));
+        exampleList.add(new patternObject(R.drawable.armillafirm_small2, "thin sweater", "Jack"));
+        exampleList.add(new patternObject(R.drawable.beanies_medium2, "beanies", "Bob"));
+        setUpRecyclerView(); //method initializes and sets the recyclerview, adapter, and layout manager
+
         return root;
     }
 
@@ -31,5 +49,15 @@ public class HomeFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public void setUpRecyclerView() {
+        mRecyclerView = binding.recyclerView; //initialize the RecyclerView from the binding object
+        mRecyclerView.setHasFixedSize(true); //the size of the RecyclerView will remain constant and won't change as the contents of the adapter change
+        mLayoutManager = new LinearLayoutManager(getContext()); //LinearLayoutManager lays out items in a RecyclerView in a vertical fashion
+        mAdapter = new cardAdapter(exampleList); //A cardAdapter is a custom adapter class that provides the data to the RecyclerView to display.
+        //The exampleList is passed to the constructor and contains the list of pattern objects that will be displayed in the RecyclerView.
+        mRecyclerView.setLayoutManager(mLayoutManager); //ensures that the RecyclerView knows how to lay out its items
+        mRecyclerView.setAdapter(mAdapter); //sets adapter
     }
 }
