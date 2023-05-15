cfc662d308b8196e929b07ee62c2ab37e1def45c
judy1wagner <judyranae@yahoo.com>
2023-05-15 10:15:11 -0700

    feat: userviewmodel add get update

--- app/src/main/java/com/yarnify/viewmodel/UserViewModel.java
+++ app/src/main/java/com/yarnify/viewmodel/UserViewModel.java
@@ -1,4 +1,31 @@
 package com.yarnify.viewmodel;
 
-public class UserViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import com.yarnify.model.User;
+import com.yarnify.repo.Repository;
+
+public class UserViewModel extends AndroidViewModel {
+
+    //access the Repository in the constructor of this viewModel
+    private Repository repository;
+    public UserViewModel(Application application){
+        super(application);
+        repository = Repository.getInstance(application.getApplicationContext());
+    }
+
+    public void addUser(User user) {
+       repository.addUser(user);
+    }
+
+    public LiveData<User> getUser(long userId) {
+        return repository.getUser(userId);
+    }
+
+    public void updateUser(User user) {
+        repository.updateUser(user);
+    }
 }
