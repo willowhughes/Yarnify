f6708b77519a7c84f7f6efc3c7f8f96b67b92cce
Alex <77035898+Dunsto@users.noreply.github.com>
2023-05-29 10:54:07 -0700

    Removed Retrofit, added a Request, response models

--- app/src/test/java/com/example/yarnify/ExampleUnitTest.java
+++ app/src/test/java/com/example/yarnify/ExampleUnitTest.java
@@ -1,21 +1,6 @@
 package com.example.yarnify;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.Request;
-import org.mockito.Mock;
-import org.mockito.MockedConstruction;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
-
 import android.test.mock.*;
-
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.when;
-
-import android.content.Context;
-
-import com.AlexUtils.AlexUtils;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
