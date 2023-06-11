27eddb2a98bdc78e387045d77b136ad2ced718a3
judy1wagner <judyranae@yahoo.com>
2023-06-10 21:47:07 -0700

    feat: add back button to yarn list

--- app/src/main/java/com/yarnify/YarnListActivity.java
+++ app/src/main/java/com/yarnify/YarnListActivity.java
@@ -1,14 +1,40 @@
 package com.yarnify;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import com.example.yarnify.R;
+import com.yarnify.model.Yarn;
+import com.yarnify.viewmodel.YarnViewModel;
+
+import java.util.ArrayList;
 
 public class YarnListActivity extends AppCompatActivity {
+
+    private ArrayList<Yarn> allYarns = new ArrayList<>();
+    private YarnViewModel yarnViewModel;
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yarn_list);
+        setBackButton();
+    }
+    public void setBackButton() {
+        // Get the support action bar
+        ActionBar actionBar = getSupportActionBar();
+        // Set the title
+        actionBar.setTitle("Yarns");
+        // Enable the back button
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        // Set a custom drawable for the up indicator to change its color
+        Drawable upArrow = getResources().getDrawable(R.drawable.baseline_arrow_back_24);
+        upArrow.setColorFilter(getResources().getColor(R.color.light_gray), PorterDuff.Mode.SRC_ATOP);
+        actionBar.setHomeAsUpIndicator(upArrow);
     }
 }
